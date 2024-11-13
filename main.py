b3fc456ed8f8d6f2a17db89866dd009701a9399d
hashyyyy <arturyleoja@gmail.com>
2024-11-13 19:36:14 +0200

    lisasin tutvustava kommentaari

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+################################################
+# Programmeerimine I
+# 2024/2025 sügissemester
+#
+# Projekt
+# Teema:
+# 
+# Lihtne muusikamängija kasutades pygame, dearpygui ja tkinteri teeke
+# Proovime lisada playlistide, equalizeri ja ka pealkirjast metaandmete 
+# võtmise funktsioonid. See on esialgne plaan, aga kui peaksime hoogu sattuma, teeme rohkem
+# Praegu on GUI väga kole, aga loodetavasti teeme selle talutavaks
+#
+# Autorid:
+# Madis Miikael Arro 
+# Artur Üleoja
+#
+# mõningane eeskuju:
+#
+# Puudub, teeme tunde järgi
+#
+# Lisakommentaar (nt käivitusjuhend):
+# 
+# Jooksuta main.py fail
+##################################################
+
 import gui.gui as gui
 
 if __name__ == "__main__":
